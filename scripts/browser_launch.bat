28b2ce77ae3b624f6b4458a5af419f9b768dc72f
Rziedreo145 <jack.olsen@gmail.com>
2018-10-14 19:54:12 -0700

    Hacky way of closing browser after scripts complete

--- scripts/browser_launch.bat
+++ scripts/browser_launch.bat
@@ -1,1 +1,3 @@
-start "" "https://accounts.intuit.com/index.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%%3A%%2F%%2Fmint.intuit.com%%2Foverview.event%%3Futm_medium%%3Ddirect%%26cta%%3Dnav_login_dropdown"
+start chrome.exe -new-window "https://accounts.intuit.com/index.html?offering_id=Intuit.ifs.mint&namespace_id=50000026&redirect_url=https%%3A%%2F%%2Fmint.intuit.com%%2Foverview.event%%3Futm_medium%%3Ddirect%%26cta%%3Dnav_login_dropdown"
+timeout /T 30 /nobreak >nul
+taskkill /IM chrome.exe /FI "WINDOWTITLE eq Mint*"
